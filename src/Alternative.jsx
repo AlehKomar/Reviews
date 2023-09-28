6f58c51e3183ed1ffe10957edc46f55fbee64577
Aleh Komar <komar.aleh@gmail.com>
2023-09-28 12:09:59 +0200

    188

--- src/Alternative.jsx
+++ src/Alternative.jsx
@@ -6,17 +6,27 @@ const App = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+
   const nextPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = (currentIndex + 1) % people.length
-      return newIndex
+      const newIndex = currentIndex + 1
+      return checkNumber(newIndex)
     })
   }
 
   const prevPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = (currentIndex - 1 + people.length) % people.length
-      return newIndex
+      const newIndex = currentIndex - 1
+      return checkNumber(newIndex)
     })
   }
 
@@ -25,8 +35,8 @@ const App = () => {
     if (randomIndex === index) {
       randomIndex = index + 1
     }
-
-    setIndex(randomIndex % people.length)
+    setIndex(checkNumber(randomIndex))
+    console.log(randomIndex)
   }
 
   return (
